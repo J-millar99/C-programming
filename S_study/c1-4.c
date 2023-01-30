e185d032874e4d1b1840de8f8a7bce6e0e0e9d90
J-millar99 <wlwogus33@gmail.com>
2023-01-30 17:31:33 +0900

    2023-01-30

--- S_study/c1-4.c
+++ S_study/c1-4.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 
+const int BREAD=500;
+const int SNACK=700;
+const int DRINK=400;
+
 int main(void)
 {   
-    int mny = 3500;
+    int mny;
     int i,j,k;
-    printf("현재 당신이 소유하고 있는 금액: %d \n", mny);
-    for(i=1; i<7; i++)
-        for(j=1; j<5; j++)
-            for(k=1; k<8; k++)
-                if(500*i + 700*j + 400*k == 3500)
+    printf("현재 당신이 소유하고 있는 금액: ");
+    scanf("%d", &mny);
+    for(i=1; i<mny/BREAD; i++)
+        for(j=1; j<mny/SNACK; j++)
+            for(k=1; k<mny/DRINK; k++)
+                if(BREAD*i+SNACK*j+DRINK*k==mny)
                     printf("크림빵 %d개, 새우깡 %d개, 콜라 %d개 \n", i,j,k);
     printf("어떻게 구입하시겠습니까? \n");
     return 0;
